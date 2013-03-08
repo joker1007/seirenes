509fa0f96045b078a76b731f98fb74e79c3e6a8e
joker1007 <kakyoin.hierophant@gmail.com>
2013-03-08 16:12:49 +0900

    Add solr41 configs

--- app/models/pasokara.rb
+++ app/models/pasokara.rb
@@ -2,15 +2,18 @@ class Pasokara < ActiveRecord::Base
   include SimpleTaggable
 
   searchable do
-    text :title
-    string :tags, multiple: true do
+    text :title, stored: true
+    string :title_sort do
+      title
+    end
+    string :tags, multiple: true, stored: true do
       tags.map(&:name)
     end
-    string :nico_vid
-    integer :nico_view_count
-    integer :nico_mylist_count
-    text :nico_description
-    time :nico_posted_at
-    integer :duration
+    string :nico_vid, stored: true
+    integer :nico_view_count, trie: true
+    integer :nico_mylist_count, trie: true
+    text :nico_description, stored: true
+    time :nico_posted_at, trie: true
+    integer :duration, trie: true
   end
 end
