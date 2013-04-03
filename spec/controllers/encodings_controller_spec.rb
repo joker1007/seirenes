14e2e3a668527f2641b0bbcda2d9c79ad6f1b178
joker1007 <kakyoin.hierophant@gmail.com>
2013-04-04 02:44:48 +0900

    Fix encodings_controller_spec

--- spec/controllers/encodings_controller_spec.rb
+++ spec/controllers/encodings_controller_spec.rb
@@ -7,7 +7,7 @@ describe EncodingsController do
     it "Add Pasokara encode queue" do
       Pasokara.should_receive(:find).with(pasokara.id.to_s).and_return(pasokara)
       pasokara.should_receive(:encode_async)
-      post :create, pasokara_id: 1
+      post :create, pasokara_id: pasokara.id
     end
   end
 end
