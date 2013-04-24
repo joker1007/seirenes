a9344640a548c187b6aa303facfebdf99f5892d1
joker1007 <kakyoin.hierophant@gmail.com>
2013-04-25 01:44:32 +0900

    [closes #3] Implement tag stop words

--- app/controllers/facet_tags_controller.rb
+++ app/controllers/facet_tags_controller.rb
@@ -5,9 +5,8 @@ class FacetTagsController < ApplicationController
     keyword = params[:q].present? ? params[:q].split("\s").map{|word| "\"#{word}\""}.join(" ") : nil
     search = Pasokara.search_with_facet_tags(SearchParameter.new(keyword: keyword, tags: params[:filter_tags]))
 
-    filters = %w(ニコニコカラオケDB ニコニコカラオケＤＢ VOCALOID ボカロカラオケDB ボカロカラオケＤＢ ニコカラ vocaloid 音楽 ﾆｺﾆｺカラオケDB 東方 カラオケ)
     tag_keyword = params[:tagq].presence
-    filtered_facet_tags = search.facet(:tags).rows.lazy.reject {|facet| facet.value.in?(filters)}
+    filtered_facet_tags = search.facet(:tags).rows.lazy.reject {|facet| facet.value.in?(Settings.stop_tag_words)}
     @facet_tags = tag_keyword ? filtered_facet_tags.select {|facet| facet.value[tag_keyword]}.take(50) : filtered_facet_tags.take(50)
   end
 end
