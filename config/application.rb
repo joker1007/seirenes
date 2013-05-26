de9951fea1240d6897ddb8af8106f26b33918c0a
joker1007 <kakyoin.hierophant@gmail.com>
2013-05-26 23:10:28 +0900

    Fix precompile setting

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,7 @@ module Seirenes
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = 'ja'
+
+    config.assets.precompile += %w( recorderWorker.js )
   end
 end
