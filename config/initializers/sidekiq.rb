f7a6a9e83b769c88ae00c6ab6aa6ecba29057d91
joker1007 <kakyoin.hierophant@gmail.com>
2014-04-14 01:21:12 +0900

    FIX redis initializer

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,5 @@
 redis_host = ENV["REDIS_PORT_6379_TCP_ADDR"] || ENV["REDIS_HOST"] || "localhost"
-redis_port = ENV["REDIS_PORT_6379_PORT"] || ENV["REDIS_PORT"] || 6379
+redis_port = ENV["REDIS_PORT_6379_TCP_PORT"] || ENV["REDIS_PORT"] || 6379
 redis_url = ENV["REDIS_URL"] || "redis://#{redis_host}:#{redis_port}/"
 
 Sidekiq.configure_server do |config|
