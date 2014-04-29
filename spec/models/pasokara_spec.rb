626533eb5c0b7276173abf1723fff7b55a3943a9
joker1007 <kakyoin.hierophant@gmail.com>
2014-04-29 23:50:42 +0900

    Remove no need tests

--- spec/models/pasokara_spec.rb
+++ spec/models/pasokara_spec.rb
@@ -1,96 +1,7 @@
 require 'spec_helper'
 
-describe Pasokara do
+describe Pasokara, elasticsearch: true do
   let(:pasokara) { create(:pasokara) }
-
-  describe "SimpleTaggable" do
-    describe "#tag_list=" do
-      it "should accept tags Array" do
-        pasokara.tag_list.should eq []
-        pasokara.tag_list = ["tag1", "tag2"]
-        pasokara.tag_list.should eq ["tag1", "tag2"]
-        pasokara.tag_list.should be_instance_of(TagList)
-      end
-
-      it "should accept tags Strings" do
-        pasokara.tag_list.should eq []
-        pasokara.tag_list = "tag1", "tag2"
-        pasokara.tag_list.should eq %w(tag1 tag2)
-        pasokara.tag_list.should be_instance_of(TagList)
-      end
-    end
-
-    describe "#add_tag" do
-      before do
-        pasokara.tag_list = ["tag1", "tag2"]
-        pasokara.tag_list.should eq ["tag1", "tag2"]
-      end
-
-      it "should add tag to tag_list" do
-        pasokara.add_tag("tag3", "tag4")
-        pasokara.tag_list.should eq %w(tag1 tag2 tag3 tag4)
-      end
-
-      it "should keep tag_list unique" do
-        pasokara.add_tag("tag1")
-        pasokara.tag_list.should eq %w(tag1 tag2)
-      end
-    end
-
-    context "After saving" do
-      it "should save own tags" do
-        pasokara.tag_list = %w(tag1 tag2)
-        pasokara.save
-        pasokara.tags.pluck(:name).should eq %w(tag1 tag2)
-      end
-
-      it "should add existing tag" do
-        Tag.create(name: "tag1")
-        pasokara.tag_list = %w(tag1 tag2)
-        pasokara.save
-        pasokara.tags.pluck(:name).should eq %w(tag1 tag2)
-      end
-
-      it "should destroy relation with no used tags" do
-        pasokara.tag_list = %w(tag1 tag2)
-        pasokara.save
-        pasokara.tags.pluck(:name).should eq %w(tag1 tag2)
-        pasokara.tag_list.delete("tag2")
-        pasokara.save
-        pasokara.tags.pluck(:name).should eq %w(tag1)
-      end
-    end
-
-    describe ".tagged_with(tags)" do
-      let(:pasokara2) { FactoryGirl.create(:pasokara, fullpath: "other path") }
-
-      before do
-        pasokara.tag_list = %w(tag1 tag2)
-        pasokara.save
-        pasokara2.tag_list = %w(tag2)
-        pasokara2.save
-      end
-
-
-      context "given tag1" do
-        subject { Pasokara.tagged_with("tag1") }
-
-        it { should eq [pasokara] }
-      end
-
-      context "given tag2" do
-        subject { Pasokara.tagged_with("tag2") }
-
-        it { should eq [pasokara, pasokara2] }
-      end
-
-      context "given tag1 and tag2" do
-        subject { Pasokara.tagged_with("tag1", "tag2") }
-
-        it { should eq [pasokara, pasokara2] }
-      end
-    end
-  end
 
   describe ".create_by_movie_info" do
     let(:info_file) { Rails.root + "spec/datas/testdir1/test002_info.xml" }
@@ -114,7 +25,7 @@ describe Pasokara do
     its(:title) { should eq movie_info.title }
     its(:duration) { should eq 205 }
     its(:nico_posted_at) { should eq Time.zone.local(2011, 7, 21, 3, 29, 1) }
-    its(:tags) { should eq %w(vocaloid tag2).map {|n| Tag.new(name: n)} }
+    its(:tags) { should match_array SimpleTaggable::Models::Tag.where(name: %w(vocaloid tag2)) }
   end
 
   describe ".create_by_movie_file" do
@@ -129,7 +40,7 @@ describe Pasokara do
     its(:fullpath) { should eq (Rails.root + "spec/datas/testdir1/test002.mp4").to_s }
     its(:duration) { should eq 205 }
     its(:nico_posted_at) { should eq Time.zone.local(2011, 7, 21, 3, 29, 1) }
-    its(:tags) { should eq %w(vocaloid tag2).map {|n| Tag.new(name: n)} }
+    its(:tags) { should match_array SimpleTaggable::Models::Tag.where(name: %w(vocaloid tag2)) }
   end
 
   describe "#favorited_by?" do
@@ -160,7 +71,7 @@ describe Pasokara do
     end
   end
 
-  describe "ElasticSearch", elasticsearch: true do
+  describe "ElasticSearch" do
     let(:pasokara) { create(:pasokara, title: "日本語混じりのTitle") }
     let(:pasokara2) { create(:pasokara, :with_other_file, title: "Other Title") }
 
--- spec/models/tag_list_spec.rb
+++
@@ -1,7 +0,0 @@
-describe TagList do
-  subject { TagList.new("tag1", "tag2", "tag1") }
-
-  it "uniq tag list" do
-    should eq ["tag1", "tag2"]
-  end
-end
